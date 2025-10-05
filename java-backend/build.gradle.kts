b50925498c9d0c1c4ce6cc15391345ca6c42d284
Polkhovskiy <Polkhovskiy@lanit.ru>
2025-10-05 18:38:09 +0300

    Added health check for java

--- java-backend/build.gradle.kts
+++ java-backend/build.gradle.kts
@@ -26,6 +26,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 
     compileOnly("org.projectlombok:lombok")
