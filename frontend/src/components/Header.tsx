4f6fc3f6a19b5922ba93ff5e74c43b58a40bd702
yaricheck <yaroslav.d.chernik@gmail.com>
2025-10-15 22:42:18 +0300

    feat: signin

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,22 +1,25 @@
 import { useUserStore } from '@/store';
+import { Button } from '@headlessui/react';
+import { useState } from 'react';
+import AuthModal from './AuthModal';
 
 const Header = () => {
   const { user } = useUserStore();
+  const [isOpenedAuthModal, setIsOpenedAuthModal] = useState(false);
   return (
     <div className="h-12 p-2 border-b border-gray-300 bg-white flex items-center justify-between">
       Header
       <div className="flex items-center gap-2">
         {!user && (
-          <>
-            <a href="/app/signin">
-              <button className="bg-gray-300 hover:bg-gray-400 px-4 py-2 rounded">Sign In</button>
-            </a>
-            <a href="/app/signup">
-              <button className="bg-gray-300 hover:bg-gray-400 px-4 py-2 rounded">Sign Up</button>
-            </a>
-          </>
+          <Button
+            className="bg-gray-300 hover:bg-gray-400 px-4 py-2 rounded"
+            onClick={() => setIsOpenedAuthModal(true)}
+          >
+            Sign In
+          </Button>
         )}
         {user && user.username && <p className="text-sm font-medium">{user.username}</p>}
+        {isOpenedAuthModal && <AuthModal onClose={() => setIsOpenedAuthModal(false)} />}
       </div>
     </div>
   );
